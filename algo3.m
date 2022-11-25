d0077f6315c00721a7c89e7fdfa9babf949fdaa3
Pablodiaz00 <58602351+Pablodiaz00@users.noreply.github.com>
2022-11-25 17:18:34 +0100

    Update algo3.m
    cositas

--- algo3.m
+++ algo3.m
@@ -1,11 +1,12 @@
-
+%% Función que crea las máscaras de un grupo de imágenes(imágenes de validación) y las guarda en "./new_masks". %%
+%% Como argumentos se le pasa un treshold para tomar la decisión, los histogramas y los Edges. %%
 function algo3 (treshold, xv, yv, histCountPiel, histCountFondo)
-
+    %% Nos colocamos en el directorio de trabajo y cargamos las imágenes de validación. %%
     cd ('.\images_val'); 
     list_validation = dir ('*.jpg');
     images_val = cell(1, length(list_validation));
     image_prueba = cell(1, length(list_validation));
-    mask_file_val = cell (1,length(list_validation));
+    mask_file_val = cell (1,length(list_validation)); 
 
     for k = 1 : length (list_validation)
         image_val = imread(list_validation(k).name);
@@ -20,7 +21,8 @@ function algo3 (treshold, xv, yv, histCountPiel, histCountFondo)
         masks_file_val = imread(list_masks_val(i).name);
         mask_file_val{i}=int8(masks_file_val);
     end
-
+    
+    %% Creamos las máscaras para cada imagen de validación y guardamos el resultado. %% 
     for k = 1 :  length(list_validation)
         cd ('..');
         [mask_image] = algo2(images_val{k}, treshold, xv, yv, histCountPiel, histCountFondo);
